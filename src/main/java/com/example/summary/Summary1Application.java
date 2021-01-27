1bd45041a5dd672a77cf9bd85372121cb998fa56
Vladimir Vishov <vishov.vladimir@gmail.com>
2021-01-27 16:24:02 +0300

    git start

---
+++ src/main/java/com/example/summary/Summary1Application.java
@@ -0,0 +1,13 @@
+package com.example.summary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summary1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summary1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/summary/Summary1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.summary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summary1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
